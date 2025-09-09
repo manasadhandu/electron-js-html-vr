31889d07f62422ccc16681c257428181866260d8
manasadhandu <manasadhandu04@gmail.com>
2025-09-09 12:22:16 +0530

    Add .gitattributes for consistent line endings

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+
+app.commandLine.appendSwitch("enable-experimental-web-platform-features");
+app.commandLine.appendSwitch("enable-webxr");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
